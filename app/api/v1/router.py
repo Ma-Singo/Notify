c706b66465b14b59b1983feece0e6d43af5102cb
Ma-Singo <codesingo@gmail.com>
2026-05-01 08:27:09 +0100

    update: added the subscription route to routers

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import users, auth, notifications
-
+from app.api.v1.endpoints import users, auth, notifications, subscriptions
 
 api_router = APIRouter()
 
 api_router.include_router(users.router)
 api_router.include_router(auth.router)
 api_router.include_router(notifications.router)
+api_router.include_router(subscriptions.router)
