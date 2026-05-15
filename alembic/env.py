f23d85c05f0ff18572987a7af1f12692339807d7
Ma-Singo <codesingo@gmail.com>
2026-05-15 12:44:18 +0100

    feat: manage postgres setup and db creation

---
+++ alembic/env.py
@@ -0,0 +1,58 @@
+from sqlalchemy import pool, create_engine
+
+from alembic import context
+from logging.config import fileConfig
+
+from app.core.config import settings
+from app.db.session import Base
+
+from app.models import *  # noqa: F401
+import sys
+from pathlib import Path
+
+
+# Add the app directory to Python path
+sys.path.append(str(Path(__file__).parent.parent))
+
+config = context.config
+fileConfig(config.config_file_name)
+
+
+target_metadata = Base.metadata
+
+sync_url = str(settings.DATABASE_URL).replace("postgresql+asyncpg://", "postgresql://")
+
+
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode."""
+    context.configure(
+        url=sync_url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode with sync connection."""
+    connectable = create_engine(sync_url, poolclass=pool.NullPool)
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+    connectable.dispose()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
